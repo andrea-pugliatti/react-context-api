0fbe2ae627816c0339915eff035d90e2e12a07b1
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-18 12:20:12 +0100

    Create BudgetContext and budgetMode state. Wrap the App in the context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
@@ -6,19 +7,25 @@ import ProductPage from "./pages/ProductPage";
 import ProductsPage from "./pages/ProductsPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 
+import BudgetContext from "./contexts/BudgetContext";
+
 function App() {
+	const [budgetMode, setBudgetMode] = useState(false);
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<DefaultLayout />}>
-					<Route index element={<HomePage />} />
-					<Route path="about" element={<AboutPage />} />
-					<Route path="products" element={<ProductsPage />} />
-					<Route path="products/:id" element={<ProductPage />} />
-					<Route path="*" element={<NotFoundPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<DefaultLayout />}>
+						<Route index element={<HomePage />} />
+						<Route path="about" element={<AboutPage />} />
+						<Route path="products" element={<ProductsPage />} />
+						<Route path="products/:id" element={<ProductPage />} />
+						<Route path="*" element={<NotFoundPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</BudgetContext.Provider>
 	);
 }
 
