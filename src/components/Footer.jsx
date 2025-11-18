cc30c0fb07b64a6bfb5b8cf3638243150d9cacfa
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-18 14:58:41 +0100

    Add Footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
+import negozioImg from "../assets/img/negozio.webp";
+
 export default function Footer() {
 	return (
 		<footer>
 			<div>Ciao</div>
+			<img src={negozioImg} alt="Logo"></img>
 		</footer>
 	);
 }
