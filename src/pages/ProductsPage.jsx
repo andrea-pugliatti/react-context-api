10a7babeff16391f0a74e82116b0c871d6880462
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-18 13:14:16 +0100

    Use the BudgetContext to filter the products on screen

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,36 +1,44 @@
-import { useEffect, useState } from "react";
+/** biome-ignore-all lint/correctness/useExhaustiveDependencies: <Bug> */
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
+import BudgetContext from "../contexts/BudgetContext";
 
 export default function ProductsPage() {
 	const productsEndpoint = "https://fakestoreapi.com/products";
 
-	// {
-	// 	id: 1,
-	// 	title: "",
-	// 	price: 1,
-	// 	description: "",
-	// 	category: "",
-	// 	image: "",
-	// 	rating: {
-	// 		rate: 1,
-	// 		count: 1,
-	// 	}
-	// }
-
 	const [products, setProducts] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [filtered, setFiltered] = useState([]);
+
+	const { budgetMode } = useContext(BudgetContext);
 
 	const fetchProducts = () => {
 		fetch(productsEndpoint)
 			.then((response) => response.json())
-			.then(setProducts)
+			.then((response) => {
+				setProducts(response);
+				filterProducts(response);
+			})
 			.catch((error) => console.error(error))
 			.finally(() => setIsLoading(false));
 	};
 
+	const filterProducts = (products) => {
+		if (budgetMode) {
+			const filtered = products.filter((product) => product.price <= 30);
+			setFiltered(filtered);
+		} else {
+			setFiltered(products);
+		}
+	};
+
 	useEffect(fetchProducts, []);
+
+	useEffect(() => {
+		filterProducts(products);
+	}, [budgetMode]);
 
 	return (
 		<main>
@@ -39,7 +47,7 @@ export default function ProductsPage() {
 					{isLoading ? (
 						<Loader />
 					) : (
-						products.map((item) => (
+						filtered.map((item) => (
 							<div key={item.id} className="col">
 								<Link to={`/products/${item.id}`}>
 									<Card product={item} />
